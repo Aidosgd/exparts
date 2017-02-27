5209a13d1a6deb8693b2b6d1726ef6576774ef42
Aidos <aidosgd@gmail.com>
2017-02-27 18:48:29 +0600

    s

---
+++ wp-content/themes/exparts/header.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<link rel="icon" href="/wp-content/themes/exparts/images/favicon.jpg">
+	<title>AN INTERNATIONAL SOCIAL NETWORK FOR ROME EXPATS &amp;  DIPLOMATS - HOME</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+	<!-- Bootstrap Core CSS -->
+	<link href="/wp-content/themes/exparts/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<link href="/wp-content/themes/exparts/css/bootstrap/bootstrap.css" rel="stylesheet">
+
+	<!-- Plugin CSS -->
+	<link rel="stylesheet" href="/wp-content/themes/exparts/css/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/wp-content/themes/exparts/dist/stylesheets/superslides.css">
+
+	<link href="/wp-content/themes/exparts/css/flexslider.css" rel="stylesheet">
+
+	<!-- Theme CSS -->
+	<link href="/wp-content/themes/exparts/css/styles.css" rel="stylesheet">
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+	<!--<link href="css/full-slider.css" rel="stylesheet">-->
+
+
+
+</head>
+<body id="home" data-spy="scroll" data-target=".navbar-fixed-top">
+<div class="member-btn"><a href="shttp://eepurl.com/btjRLP" class="btn btn-info" target="_blank">Become A member</a></div>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container-fluid">
+		<div class="navbar-header page-scroll">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand page-scroll" href="#index.html"><img src="/wp-content/themes/exparts/images/logo.png" class="img-responsive"></a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav main-nav navbar-right">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="about-us.html">ABOUT US</a></li>
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">EVENTS<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="upcoming-events.html">UPCOMING EVENTS</a></li>
+						<li><a href="other-events.html">OTHER EVENTS</a></li>
+						<li><a href="gallery.html">GALLERY</a></li>
+					</ul>
+				</li>
+
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">LANGUAGE COURSES <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="italian-lessons.html">ITALIAN LESSONS  </a></li>
+						<li><a href="english-lessons.html">ENGLISH LESSONS </a></li>
+					</ul>
+				</li>
+
+
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">LOOKING FOR <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="jobs.html">JOBS </a></li>
+						<li><a href="rooms-for-rent.html">ROOMS FOR RENT</a></li>
+						<li><a href="businesses-services.html">BUSINESSES &amp; SERVICES </a></li>
+					</ul>
+				</li>
+
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">ADVERTISING  <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="sponsorships.html">SPONSORSHIPS  </a></li>
+						<li><a href="classifieds.html">CLASSIFIEDS</a></li>
+					</ul>
+				</li>
+
+
+				<li><a href="blog-press.html">BLOG &amp; PRESS </a></li>
+				<li><a href="login.html">Login</a></li>
+			</ul>
+
+		</div>
+		<!-- /.navbar-collapse -->
+	</div>
+	<!-- /.container -->
+</nav>
---
+++ wp-content/themes/exparts/js/main.js
@@ -0,0 +1,60 @@
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('li.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+equalheight = function(container){
+
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
+ $(container).each(function() {
+
+   $el = $(this);
+   $($el).height('auto')
+   topPostion = $el.position().top;
+
+   if (currentRowStart != topPostion) {
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+   } else {
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+  }
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+ });
+}
+
+$(window).load(function() {
+  equalheight('#Whatwetreat-section .col-lg-5ths, .BlogCont .list-group-item');
+});
+
+
+$(window).resize(function(){
+  equalheight('#Whatwetreat-section .col-lg-5ths, .BlogCont .list-group-item');
+});
+
